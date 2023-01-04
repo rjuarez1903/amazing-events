189ac8fe1c2aa397dab180a9307ca17643ebc0af
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-01-04 12:35:03 -0300

    refactor getStats() function

--- js/stats.js
+++ js/stats.js
@@ -22,14 +22,24 @@ function getCategories(events) {
 
 function getStats(events, categories) {
     let stats = []
+    let categoryStats = []
     events.map(event => {
         stats.push({
             eventName: event.name,
+            category: event.category,
             percentageOfAssistance: (((event.assistance || event.estimate) * 100) / event.capacity).toFixed(2), 
             revenue: ((event.assistance || event.estimate) * event.price)
         })
     })
     console.log(stats)
+    stats.map(stat => {
+        categoryStats.push({
+            category: stat.category, 
+            revenue: stat.revenue,
+            percentageOfAssistance: stat.percentageOfAssistance
+        })
+    })
+    console.log(categoryStats)
 }
 
 getData()
