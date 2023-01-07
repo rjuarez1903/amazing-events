7004dcb9405ba56936d1dc4c2eb0000f76f4da14
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-01-07 14:21:50 -0300

    change card layout

--- js/details.js
+++ js/details.js
@@ -20,19 +20,26 @@ fetch('https://mindhub-xj03.onrender.com/api/amazing')
 const renderCard = (event) => {
     cardDetailContainer.innerHTML = 
     `
-    <div class="card-detail row rounded m-1">
+    <div class="card-detail row rounded m-1 d-flex align-items-center">
         <div class="col-lg-6 card-details-img-container d-flex align-items-center">
-            <img src="${event.image}" alt="${event.name}" class="w-100">
+            <img src="${event.image}" alt="${event.name}" class="w-100 rounded">
         </div>
         <div class="col-lg-6 p-4">
             <h1 class="fw-bold text-center" id="card-detail-title">${event.name}</h1>
-            <small class="rounded-pill my-1 cinema">${event.category}</small>
-            <small class="rounded-pill my-1 bg-success">Date: ${event.date}</small>
+            <div class="d-flex my-1 justify-content-between flex-wrap align-items-center w-100 gap-2">
+                <small class="rounded-pill cinema">${event.category}</small>
+                <div class="d-flex align-items-center">
+                    <i class="fa-regular fa-calendar-days"></i> 
+                    <small>${event.date}</small>
+                </div>
+            </div>
             <p class="mt-2">${event.description}</p>
-            <p>Place: ${event.place}</p>
-            <p>Capacity: ${event.capacity}</p>
-            <p>${event.assistante ? 'Assistance' : 'Estimate'}: ${event.assistance || event.estimate}</p>
-            <p>Price: $${event.price}</p>
+                <div class="row">
+                    <p class="col-sm-6 text-center">Place: <span>${event.place}</span></p>
+                    <p class="col-sm-6 text-center">Capacity: ${event.capacity}</p>
+                    <p class="col-sm-6 text-center">${event.assistante ? 'Assistance' : 'Estimate'}: ${event.assistance || event.estimate}</p>
+                    <p class="col-sm-6 text-center">Price: $${event.price}</p>
+                </div>
         </div>
     </div>
     `
