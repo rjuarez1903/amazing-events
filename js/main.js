544c6eed6e39918b1821b493995678d247e8f89f
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-01-07 02:42:11 -0300

    add createCard() and renderCards() functions

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ fetch('https://mindhub-xj03.onrender.com/api/amazing')
     .then(json => {
         events = json.events
         renderCategories(events, checkboxContainer)
+        renderCards(events, cardsContainer)
     })
     .catch(err => console.log(err))
 
@@ -28,6 +29,58 @@ const renderCategories = (events, checkboxContainer) => {
         `
     })
     checkboxContainer.innerHTML = checkboxContainerHtml
+}
+
+const createCard = (event) => {
+    // Format event.category to use it as a CSS class
+    let categoryDashed = (event.category).replace(/\s+/g, '-').toLowerCase()
+
+    // HTML template creation
+    let card           = document.createElement('div')
+    let div            = document.createElement('div')
+    let img            = document.createElement('img')
+    let cardBody       = document.createElement('div')
+    let titleCategory  = document.createElement('div')
+    let cardTitle      = document.createElement('h5')
+    let categoryPill   = document.createElement('small')
+    let cardText       = document.createElement('p')
+    let cardFooter     = document.createElement('div')
+    let price          = document.createElement('price')
+    let a              = document.createElement('a')
+
+    card.className           = 'col-xl-3 col-lg-4 col-sm-6'
+    div.className            = 'card rounded'
+    div.style.width          = '100%' 
+    img.src                  = `${event.image}` 
+    img.className            = 'card-img-top'
+    img.alt                  = `${event.name}`
+    cardBody.className       = 'card-body d-flex flex-column justify-content-between'
+    titleCategory.className  = 'title-category'
+    cardTitle.className      = `card-title fw-bold text-center`
+    cardTitle.textContent    = `${event.name}`
+    categoryPill.className   = `rounded-pill my-1 ${categoryDashed}`
+    categoryPill.textContent = `${event.category}`
+    cardText.textContent     = `${event.description}`
+    cardFooter.className     = 'd-flex justify-content-between align-items-center flex-wrap gap-1'
+    price.textContent        =  `Price: $${event.price} `
+    a.href                   = `./details.html?id=${event._id}`
+    a.className              = 'btn btn-custom w-100'
+    a.textContent            = 'Show details'
+
+    card.append(div)
+    div.append(img, cardBody)
+    cardBody.append(titleCategory, cardText, cardFooter)
+    titleCategory.append(cardTitle, categoryPill)
+    cardFooter.append(price, a)
+
+    return card
+
+}
+
+const renderCards = (events, container) => {
+    let fragment = document.createDocumentFragment()
+    events.forEach(event => fragment.appendChild(createCard(event)))
+    container.appendChild(fragment)
 }
 
 // async function renderCards(cards) {
