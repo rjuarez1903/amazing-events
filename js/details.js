c65c04538fd92dc1c495e9cae51940c836464584
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-01-08 01:45:47 -0300

    card detail redesign

--- js/details.js
+++ js/details.js
@@ -12,34 +12,42 @@ fetch('https://mindhub-xj03.onrender.com/api/amazing')
 })
 .catch(err => console.log(err))
 .finally(() => {
+    loading.classList.add('d-none')
+    cardDetailContainer.classList.remove('d-none')
     document.body.classList.add('bg-grey-gradient')
     var tl = gsap.timeline({defaults: {duration: 1 }})
     tl.from("#card-detail-container", {y: 50, opacity: 0})
 })
 
 const renderCard = (event) => {
+    let categoryDashed = event.category.replace(/\s+/g, '-').toLowerCase()
     cardDetailContainer.innerHTML = 
     `
-    <div class="card-detail row rounded m-1 d-flex align-items-center">
-        <div class="col-lg-6 card-details-img-container d-flex align-items-center">
-            <img src="${event.image}" alt="${event.name}" class="w-100 rounded">
-        </div>
-        <div class="col-lg-6 p-4">
-            <h1 class="fw-bold text-center" id="card-detail-title">${event.name}</h1>
-            <div class="d-flex my-1 justify-content-between flex-wrap align-items-center w-100 gap-2">
-                <small class="rounded-pill cinema">${event.category}</small>
-                <div class="d-flex align-items-center">
-                    <i class="fa-regular fa-calendar-days"></i> 
-                    <small>${event.date}</small>
+    <div class="card-detail row rounded m-1">
+        <img src="${event.image}" alt="${event.name}" class="col-lg-4 col-md-12" id="card-img-detail">
+        <div class="col-lg-6 col-md-9 p-4 d-flex flex-column justify-content-center">
+            <h1 class="fw-bold text-center mb-4" id="card-detail-title">${event.name}</h1>
+            <div class="d-flex my-1 justify-content-between flex-wrap align-items-center w-100 gap-4 mb-4">
+                <small class="rounded-pill ${categoryDashed}">${event.category}</small>
+                <div class="d-flex flex-wrap gap-4">
+                    <div class="d-flex align-items-center gap-1">
+                        <i class="fa-solid fa-location-dot card-icon"></i>
+                        <small class="p-0">${event.place}</small>
+                    </div>
+                    <div class="d-flex align-items-center gap-1">
+                        <i class="fa-regular fa-calendar-days card-icon"></i> 
+                        <small class="p-0">${event.date}</small>
+                    </div>
                 </div>
             </div>
             <p class="mt-2">${event.description}</p>
-                <div class="row">
-                    <p class="col-sm-6 text-center">Place: <span>${event.place}</span></p>
-                    <p class="col-sm-6 text-center">Capacity: ${event.capacity}</p>
-                    <p class="col-sm-6 text-center">${event.assistante ? 'Assistance' : 'Estimate'}: ${event.assistance || event.estimate}</p>
-                    <p class="col-sm-6 text-center">Price: $${event.price}</p>
-                </div>
+            <p class="mt-2 fw-bold my-0 fs-3 price-detail text-center">$${event.price}</p>
+        </div>
+        <div class="card-detail-footer col-lg-2 col-md-3 p-4 ${categoryDashed}">
+            <div class="row h-100 align-items-center">
+                <p class="col-lg-12 text-center">Capacity: <span class="fw-bold">${event.capacity}<span></p>
+                <p class="col text-center">${event.assistante ? 'Assistance' : 'Estimate'}: <span class="fw-bold">${event.assistance || event.estimate}</p>
+            </div>            
         </div>
     </div>
     `
