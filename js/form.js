adde9b235377052344bc416252430786536f80f7
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-01-09 21:28:37 -0300

    add animateHeader() function

--- js/form.js
+++ js/form.js
@@ -1,7 +1,13 @@
 const tl = gsap.timeline({defaults: {duration: 1 }})
 
-const animateForm = () => {
-    tl.from('form', {x: 50, opacity: 0})
+const animateHeader = () => {
+    tl.from('.navbar-brand', {x: -50, opacity: 0})
+    tl.from('.navbar-nav', {x: 50, opacity: 0}, "-=1")
 }
 
+const animateForm = () => {
+    tl.from('form', {x: 50, opacity: 0}, "-=1")
+}
+
+animateHeader()
 animateForm()
